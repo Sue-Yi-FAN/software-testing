9144f11ec7cd7140af698a3dfd9d109c1eb6cb80
Sue-Yi-FAN <x95102003@gmail.com>
2015-11-26 11:34:13 +0800

    take apart the struct of triangle and its test

---
+++ homework2/triangle.cc
@@ -0,0 +1,24 @@
+#include "triangle.h"
+bool isATriangle(int a,  int b,  int c){
+	if ((a < b + c) && (b < a + c) && (c < a +b) 
+		&& (a > 0) && (b>0) && (c >0)) 
+		return true;
+	return false;
+}
+
+string TriangleType(int a,  int b,  int c){
+if(isATriangle(a,  b,  c)){	
+	if ((a == b) && (b == c))
+		return "Equilateral";
+    else if ((a != b) && (a != c) && (b != c))
+		return "Scalene";
+    else 
+    	return "Isosceles";
+ }
+ else{
+ 	if (a > 200 || b > 200 || c > 200)
+		return "out of permitted range";
+	else 
+ 		return "Not a Triangle";
+	}
+}
---
+++ homework2/triangle.h
@@ -0,0 +1,7 @@
+#include <limits.h>
+#include <string.h>
+#include "gtest/gtest.h"
+
+using namespace std;
+bool isATriangle(int ,  int ,  int );
+string TriangleType(int ,  int ,  int );
