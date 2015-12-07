2da838d1e4c0aceebbcdeac3dd474dbe6e59e844
smallwhite <x95102003.cs0g@g2.nctu.edu.tw>
2015-12-07 08:45:21 -0800

    Add Cmcdc test case of date.

--- homework2/date_Cmcdc/date_Cmcdc.cc
+++ homework2/date_Cmcdc/date_Cmcdc.cc
@@ -2,33 +2,26 @@
 #include "gtest/gtest.h"
 #include "../date.h"
 
-TEST(dataTest, C1_Testcase){
-	EXPECT_EQ("Out of year range 1812-2012",  nextdate(1811, 1, 15));
-	EXPECT_EQ("Error day -1",  nextdate(1912, 1, -1));
-	EXPECT_EQ("Error day -1",  nextdate(1912, 4, -1));
-	EXPECT_EQ("Error day -1",  nextdate(1912, 2, -1));
-	EXPECT_EQ("Error day -1",  nextdate(1912, 12, -1));
-	
-	EXPECT_EQ("1912/2/1",  nextdate(1912, 1, 31));
-	EXPECT_EQ("1912/1/16",  nextdate(1912, 1, 15));
-	EXPECT_EQ("Error day 32",  nextdate(1912, 1, 32));
-
-	EXPECT_EQ("1912/5/1",  nextdate(1912, 4, 30));
-	EXPECT_EQ("1912/4/16",  nextdate(1912, 4, 15));
-	EXPECT_EQ("Error day 32",  nextdate(1912, 4, 32));
-
-	EXPECT_EQ("2000/2/28",  nextdate(2000, 2, 27));
+TEST(dataTest, Cmcdc_Testcase){
+	//fixed month and day, change year.
+	EXPECT_EQ("Out of year range 1812-2012",  nextdate(1811, 2, 28));
 	EXPECT_EQ("2000/2/29",  nextdate(2000, 2, 28));
-	EXPECT_EQ("2001/3/1",  nextdate(2001, 2, 28));
-	EXPECT_EQ("2000/3/1",  nextdate(2000, 2, 29));
-	EXPECT_EQ("Cannot have Feb 29",  nextdate(2001, 2, 29));
-	EXPECT_EQ("Error day 32", nextdate(1912, 2, 32));
+	EXPECT_EQ("1912/2/29",  nextdate(1912, 2, 28));
+	EXPECT_EQ("1913/3/1",  nextdate(1913, 2, 28));
 
-	EXPECT_EQ("1913/1/1",  nextdate(1912, 12, 31));
-	EXPECT_EQ("1912/12/16",  nextdate(1912, 12, 15));
-	EXPECT_EQ("Error day 32",  nextdate(1912, 12, 32));
+	//fixed years and day, change month.
+	EXPECT_EQ("2000/2/1",  nextdate(2000, 1, 31));
+	EXPECT_EQ("Error day 31",  nextdate(2000, 2, 31));
+	EXPECT_EQ("Error day 31",  nextdate(2000, 9, 31));
+	EXPECT_EQ("2001/1/1",  nextdate(2000, 12, 31));
+	EXPECT_EQ("Error month 13",  nextdate(2000, 13, 31));
 
-	EXPECT_EQ("Error month 13",  nextdate(1912, 13, 1));
-	EXPECT_EQ("Out of year range 1812-2012",  nextdate(2013, 1, 1));
+	//fixed years and month, change day.
+	EXPECT_EQ("1933/5/1",  nextdate(1933, 4, 30));
+	EXPECT_EQ("Error day 31",  nextdate(1933, 4, 31));
+	EXPECT_EQ("1933/4/30",  nextdate(1933, 4, 29));
+	EXPECT_EQ("1933/4/2",  nextdate(1933, 4, 1));
+	EXPECT_EQ("Error day 0",  nextdate(1933, 4, 0));
+
 }
 
