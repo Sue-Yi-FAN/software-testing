701f3dd7d9ead2922dcfa071eb59df48739f00d2
smallwhite <x95102003.cs0g@g2.nctu.edu.tw>
2015-12-07 07:59:15 -0800

    Add Cmcdc test case of triangle.

--- homework2/triangle_Cmcdc/triangle_Cmcdc.cc
+++ homework2/triangle_Cmcdc/triangle_Cmcdc.cc
@@ -3,15 +3,25 @@
 #include "../triangle.h"
 
 TEST(TriangleType, Equivalence_Class_Test_Case){
-	EXPECT_EQ("Not a Triangle", TriangleType(-1, 5, 5));
-	EXPECT_EQ("Not a Triangle", TriangleType(5, -1, 5));
-	EXPECT_EQ("Not a Triangle", TriangleType(5, 5, -1));
-	EXPECT_EQ("Not a Triangle", TriangleType(-1, -1, 5));
-	EXPECT_EQ("Not a Triangle", TriangleType(-1, 5, -1));
-	EXPECT_EQ("Not a Triangle", TriangleType(-1, -1, 5));
-	EXPECT_EQ("Not a Triangle", TriangleType(-1, -1, -1));
-	EXPECT_EQ("out of permitted range", TriangleType(5, 5, 201));
-	EXPECT_EQ("out of permitted range", TriangleType(5, 201, 5));
-	EXPECT_EQ("out of permitted range", TriangleType(201, 5, 5));
+	//fix b, c change a
+	EXPECT_EQ("Isosceles", TriangleType(1, 195, 195 ));
+	EXPECT_EQ("Not a Triangle", TriangleType(400, 195, 195 ));
+	EXPECT_EQ("Not a Triangle", TriangleType(-1, 195,195 ));
+	EXPECT_EQ("out of permitted range", TriangleType(201, 195, 195));
+	EXPECT_EQ("Equilateral", TriangleType(195, 195, 195 ));
+
+	//fix a, b change c
+	EXPECT_EQ("Not a Triangle", TriangleType(193, 190, 400));
+	EXPECT_EQ("Not a Triangle", TriangleType(193, 190, -1));
+	EXPECT_EQ("out of permitted range", TriangleType(193, 190, 201));
+	EXPECT_EQ("Scalene", TriangleType(193, 190, 194 ));
+	EXPECT_EQ("Isosceles", TriangleType(193, 190, 193 ));
+
+	//fix a, c change b
+	EXPECT_EQ("Not a Triangle", TriangleType(180, 400, 180));
+	EXPECT_EQ("Not a Triangle", TriangleType(180, -1, 180));
+	EXPECT_EQ("out of permitted range", TriangleType(180, 201, 180));
+	EXPECT_EQ("Isosceles", TriangleType(180, 170, 180 ));
+	EXPECT_EQ("Equilateral", TriangleType(180, 180, 180 ));
 
 }
