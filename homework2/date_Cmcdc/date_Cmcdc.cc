f3e51f7f9f67a0bcc08d95f76396e8c9ce966c14
smallwhite <x95102003.cs0g@g2.nctu.edu.tw>
2015-12-07 19:22:36 -0800

    Add more test case for the branch.

--- homework2/date_Cmcdc/date_Cmcdc.cc
+++ homework2/date_Cmcdc/date_Cmcdc.cc
@@ -9,6 +9,9 @@ TEST(dataTest, Cmcdc_Testcase){
 	EXPECT_EQ("1912/2/29",  nextdate(1912, 2, 28));
 	EXPECT_EQ("1913/3/1",  nextdate(1913, 2, 28));
 
+	EXPECT_EQ("2000/3/1", nextdate(2000, 2, 29));
+	EXPECT_EQ("Cannot have Feb 29", nextdate(1993, 2, 29));
+
 	//fixed years and day, change month.
 	EXPECT_EQ("2000/2/1",  nextdate(2000, 1, 31));
 	EXPECT_EQ("Error day 31",  nextdate(2000, 2, 31));
@@ -23,5 +26,16 @@ TEST(dataTest, Cmcdc_Testcase){
 	EXPECT_EQ("1933/4/2",  nextdate(1933, 4, 1));
 	EXPECT_EQ("Error day 0",  nextdate(1933, 4, 0));
 
+	EXPECT_EQ("1933/1/2",  nextdate(1933, 1, 1));
+	EXPECT_EQ("Error day 0",  nextdate(1933, 1, 0));
+	EXPECT_EQ("Error day 32",  nextdate(1933, 1, 32));
+
+	EXPECT_EQ("1992/2/2", nextdate(1992, 2, 1));
+	EXPECT_EQ("Error day 31", nextdate(1992, 2, 31));
+	EXPECT_EQ("1992/3/1", nextdate(1992, 2, 29));
+
+	EXPECT_EQ("1910/12/2", nextdate(1910, 12, 1));
+	EXPECT_EQ("Error day 32", nextdate(1910, 12, 32));
+	EXPECT_EQ("Error day -1", nextdate(1910, 12, -1));
 }
 
